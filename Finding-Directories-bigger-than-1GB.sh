9a3b67978cf4244af6819570cc08737cb42c5814
Vahid <vhdhbb@outlook.com>
2023-09-01 15:03:12 +0930

    This is the first version of Finding Directories bigger thatn KB!

---
+++ Finding-Directories-bigger-than-1GB.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Directory to search for files
+search_directory="/var/log"
+
+# Log file to store results
+log_file="/home/vhdhbb/Desktop/large_files.log"
+
+# Iterate over files in the directory
+for file in "$search_directory"/*; do
+    if [ -f "$file" ]; then
+        size=$(stat -c %s "$file")   # Get file size in bytes
+        size_in_gb=$((size / 1024 / 1024 / 1024))   # Convert size to GB
+
+        if [ "$size_in_gb" -gt 1 ]; then
+            echo "File: $file, Size: $size_in_gb GB" >> "$log_file"
+        fi
+    fi
+done
+
+echo "Large files have been logged to $log_file"
